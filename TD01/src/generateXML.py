6cc125c53351ec379d6a8dc9e0bc20cfb016c8eb
KillianDbr <debruyne.killian.snt@gmail.com>
2026-03-05 19:25:58 +0100

    added article & bulletin

--- TD01/src/generateXML.py
+++ TD01/src/generateXML.py
@@ -2,8 +2,8 @@ import re
 from pathlib import Path
 
 """
-— le numéro de l'article,
-— le numéro du bulletin,
+— le numéro de l'article ---> ok,
+— le numéro du bulletin ---> ok,
 — la date ---> ok,
 — la rubrique ---> ok,
 — le titre de l'article ---> ok,
@@ -32,12 +32,25 @@ def extractTitle(text):
     if match:
         return match.group(3)
 
+def extractNumeroBulletin(text):
+    match = re.search(r"'Numero', '(.*?(\d{3,4}))'",text)
+    if match:
+        return match.group(2)
+
+def extractNumeroArticle(text):
+    match = re.search(r"'Code', '(.*?(\d{4,5}))'",text)
+    if match:
+        return match.group(2)
+
+def extractImages(text):
+    match = re.search(r"<p class=.style98.><img src=.http://www.bulletins-electroniques.com/Resources_fm/drapeau01/france.jpeg.")
+
 def main():
     for file in DATA_PATH.glob("*.htm"):
         with open(file, "r", encoding="utf-8") as f:
             content = f.read()
-            titre = extractTitle(content)
-            print(f"file: {file.name} --> {titre}")
+            numero = extractNumeroArticle(content)
+            print(f"file: {file.name} --> {numero}")
     
 
 if __name__ == "__main__":
